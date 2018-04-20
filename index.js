ca303f6a1f38fcc80df0b190ff8d45636f33a71f
Christian Bodine <mach656@gmail.com>
2018-04-20 15:43:16 -0500

    k

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
+
 // Global variables
 
-var blueDiv = document.getElementById('blue');
-var purpleDiv = document.getElementById('purple');
-var posB;
+var kirbyDiv = document.getElementById('kirby');
+var poppyDiv = document.getElementById('poppy');
+var posK;
 var posP;
-var blueImage = document.getElementById('checkeredFlagImg');
-var purpleImage = document.getElementById('checkeredFlagImg');
+var kirbyImage = document.getElementById('kirbyImg');
+var poppyImage = document.getElementById('poppyImg');
 
 // Resets image sources and position
 function setUp(){
-    posB = 0;
-    posP = 0;
-    blueDiv.style.left = posB + 'px';
-    purpleDiv.style.left = posP + 'px';
-    blueImage.src = "blue-car-md.png";
-    purpleImage.src = "purple-car-md.png";
-    blueImage.style.display = "block";
-    purpleImage.style.display = "block";
-    document.getElementById('checkeredFlagBlue').style.display = "none";
-    document.getElementById('startButton').style.display = "block";
-    document.getElementById('startButtonGo').style.display = "none";
+  posK = 0;
+  posP = 0;
+  kirbyDiv.style.left = posK + 'px';
+  poppyDiv.style.left = posP + 'px';
+  kirbyImage.src = "kirby_bomb.png";
+  poppyImage.src = "poppy_bomb.png";
+  kirbyImage.style.display = "block";
+  poppyImage.style.display = "block";
+  document.getElementById('kirbyWinsImg').style.display = "none";
+  document.getElementById('poppyWinsImg').style.display = "none";
+  document.getElementById('startButton').style.display = "block";
+  document.getElementById('startButtonBW').style.display = "none";
 }
 
 // Conduct "the race"
 function race() {
-    setUp();
-    document.getElementById('startButton').style.display = "none";
-    document.getElementById('startButtonGo').style.display = "block";
-    // The frame() function occurs at interval
-    var timer = setInterval(frame, 100);
-    // Moves the images to the right by a random number between 1 and 20
-    function frame (){
-        if (posB > 500 || posP > 500 ){
-            clearInterval(timer);
-            posB > posP ? blueWins() : purpleWins();
-        } else {
-            var rdmB = Math.floor(Math.random() * 20);
-            var rdmP = Math.floor(Math.random() * 20);
-            posB += rdmB;
-            posP += rdmP;
-            blueDiv.style.left = posB + 'px';
-            purpleDiv.style.left = posP + 'px';
-            
-        }
+  setUp();
+  document.getElementById('startButton').style.display = "none";
+  document.getElementById('startButtonBW').style.display = "block";
+  // The frame() function occurs at interval
+  var timer = setInterval(frame, 100);
+// Moves the images to the right by a random number between 1 and 20
+  function frame (){
+    if (posK > 500 || posP > 500 ){
+      clearInterval(timer);
+      posK > posP ? kirbyWins() : poppyWins();
+    } else {
+      var rdmK = Math.floor(Math.random() * 20);
+      var rdmP = Math.floor(Math.random() * 20);
+      posK += rdmK;
+      posP += rdmP;
+      kirbyDiv.style.left = posK + 'px';
+      poppyDiv.style.left = posP + 'px';
+
     }
+  }
 }
 
 // Makes the winning image visible & hides race image
-function blueWins(){
-    blueImage.style.display = "none";
-    document.getElementById('checkeredFlagBlue').style.display = "block";
-    
+function kirbyWins(){
+  kirbyImage.style.display = "none";
+  document.getElementById('kirbyWinsImg').style.display = "block";
+
 }
 // Makes the winning image visible & hides race image
-function purpleWins(){
-    purpleImage.style.display = "none";
-    document.getElementById('checkeredFlagBlue').style.display = "block";
+function poppyWins(){
+  poppyImage.style.display = "none";
+  document.getElementById('poppyWinsImg').style.display = "block";
 }
